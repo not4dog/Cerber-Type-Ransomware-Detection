3c8eb49795c18f1b294da68dcc78e46d289b7863
Defaulthyun <32670730+defaulthyun@users.noreply.github.com>
2022-08-10 12:57:56 +0900

    Update modeling.py

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 
