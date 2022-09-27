cd15182d2e59a7dc77407a61ebf57c8e4083b6fc
Hminchae <hminchae@gmail.com>
2022-09-27 16:49:39 +0900

    cuckoo clean과 생성된 exe,json 모두 삭제하는 코드 추가

---  temporary/dynamic_analysis.py
+++  temporary/dynamic_analysis.py
@@ -9,15 +9,15 @@ ssh.connect('192.168.40.128',port='22',username='b793170',password ='02030203')
 
 #파일전송(로컬->서버)
 sftp =ssh.open_sftp()
-remotepath = '/home/b793170/5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.exe' # sftp에 업로드 될때 파일 경로/파일이름
-localpath  = 'c:/Users/Hwang/Desktop/5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.exe' # local pc의 파일 경로/파일이름
+remotepath = '/home/b793170/filename.exe' # sftp에 업로드 될때 파일 경로/파일이름
+localpath  = 'c:/Users/Hwang/Desktop/filename.exe' # local pc의 파일 경로/파일이름
 sftp.put(localpath, remotepath)
 
-#쿠쿠분석
+#분석
 stdin, stdout, stderr = ssh.exec_command('cuckoo')
-stdin, stdout, stderr = ssh.exec_command('cuckoo api')
-stdin, stdout, stderr = ssh.exec_command("curl -H 'Authorization: Bearer pxJLRqiTfxz0PNNhGLdoew' -F 'file=@/home/b793170/Desktop/5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.exe' http://localhost:8090/tasks/create/file")
-stdin, stdout, stderr = ssh.exec_command('curl -H "Authorization: Bearer pxJLRqiTfxz0PNNhGLdoew" http://localhost:8090/tasks/report/1/json >5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.json')
+stdin, stdout, stderr = ssh.exec_command('cuckoo api')#api서버 접속
+stdin, stdout, stderr = ssh.exec_command("curl -H 'Authorization: Bearer pxJLRqiTfxz0PNNhGLdoew' -F 'file=@/home/b793170/Desktop/filename.exe' http://localhost:8090/tasks/create/file")
+stdin, stdout, stderr = ssh.exec_command('curl -H "Authorization: Bearer pxJLRqiTfxz0PNNhGLdoew" http://localhost:8090/tasks/report/1/json >filename.json')
 stdin, stdout, stderr = ssh.exec_command('cuckoo clean')
 
 
@@ -26,13 +26,12 @@ print(''.join(stdout.readlines()))
 
 
 #파일전송(서버->로컬)
-remotepath2 = '/home/b793170/5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.json'
-localpath2 = 'c:/Users/Hwang/Desktop/5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.json'
+remotepath2 = '/home/b793170/filename.json'
+localpath2 = 'c:/Users/Hwang/Desktop/filename.json'
 sftp.get(remotepath2, localpath2)
 
 #우분투 내 파일삭제
-stdin, stdout, stderr = ssh.exec_command('rm 5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.exe 5db631589b179544445b62d72e5021293408799c694c848452752a8a01517d2e.json')
-
+stdin, stdout, stderr = ssh.exec_command('rm filename.exe filename.json')
 
 ssh.close()
 sftp.close()
