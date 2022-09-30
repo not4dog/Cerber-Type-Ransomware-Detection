aa2b696988fe499b70a841d5aae36dc9570bc6ff
not4dog <gdsmsla@naver.com>
2022-09-30 23:26:31 +0900

    동적 모듈 병합 (CTRD.py)

--- GUI/Cuckoo_Analysis.py
+++ GUI/Cuckoo_Analysis.py
@@ -1,29 +1,30 @@
 import paramiko
 import json
-import time
-from GUIOpen import *
+from time import *
+from CTRD import *
 
 def sshConnect():
     global ssh
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('211.214.61.14',port='22',username='ubuntu',password ='ubuntu')
-    return
+    ssh.connect('211.214.61.14',port='2200',username='b793170',password ='20100709')
 
-def UploadedFileTransper():
-    global sftp 
-    sftp = ssh.open_sftp()
-    remotepath = '/home/ubuntu/Uploaded_exe/Scan.exe' 
-    localpath  = filename[0]
+#파일전송(로컬->서버)
+def FileTransper():
+    global sftp
+    sftp =ssh.open_sftp()
+    remotepath = '/home/b793170/Desktop/Scan.exe' # sftp에 업로드 될때 파일 경로/파일이름
+    localpath  = filepath # local pc의 파일 경로/파일이름
     sftp.put(localpath, remotepath)
-    return
 
-def CuckooAnalysis():
-    stdin, stdout, stderr = ssh.exec_command('cuckoo')
-    stdin, stdout, stderr = ssh.exec_command('cuckoo submit --timeout 90 /home/ubuntu/Uploaded_exe/Scan.exe')
-    return
+#분석
+#stdin, stdout, stderr = ssh.exec_command('cuckoo')#api서버 접속
+def Analysis():
+    stdin, stdout, stderr = ssh.exec_command("cuckoo")
+    stdin, stdout, stderr = ssh.exec_command("cuckoo submit --timeout 90 /home/b793170/Desktop/Scan.exe")
 
-def jsonTransper():
+#파일존재여부 확인
+def Exists():
     output = False
     result = False
     sec = 5
@@ -33,23 +34,18 @@ def jsonTransper():
         output =''.join(stdout.readlines())
         result = output.replace(" ","")
         json.loads(result.lower())
-        time.sleep(sec)
+        sleep(sec)
         if json.loads(result.lower()) != False :
             break
 
-    stdin, stdout, stderr = remotepath2 = '/home/ubuntu/.cuckoo/storage/analyses/1/report.json'
-    stdin, stdout, stderr = localpath2 = 'Detection_Feature_Data/{0}_API_Extract.json' .format(sha256)
-    stdin, stdout, stderr = sftp.get(remotepath2, localpath2)
-    return
+#파일전송(서버->로컬)
+def FileTransperAndRemove():
+    remotepath2 = '/home/b793170/.cuckoo/storage/analyses/1/reports/report.json'
+    localpath2 = 'Detection_Feature_Data\{0}_API_Extract.json' .format(sha256)
+    sftp.get(remotepath2, localpath2)
 
-def AnalysisFileRemove():
-    stdin, stdout, stderr = ssh.exec_command('rm -f /home/ubuntu/Uploaded_exe/Scan.exe')
-    stdin, stdout, stderr = ssh.exec_command('cuckoo clean')
-    return
+    stdin, stdout, stderr = ssh.exec_command("cuckoo clean")
+    stdin, stdout, stderr = ssh.exec_command("rm -f /home/b793170/Desktop/Scan.exe")
 
-def Exit():
     ssh.close()
     sftp.close()
-    return
-
-
